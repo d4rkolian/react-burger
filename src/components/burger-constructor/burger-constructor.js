ecc24c994bdaa0f14f433a30452ba0e88c486a7b
Doroshin Nikolay <ndoroshin@HD-MacBook-02.Dlink>
2021-07-19 14:30:36 +0300

    base layout and components

---
+++ src/components/burger-constructor/burger-constructor.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import BGStyles from './burger-constructor.module.css';
+import meatImg from '../../images/meat-02.png';
+
+import * as utils from '../../utils/data.js';
+
+class Ingredient extends React.Component {
+  render() {
+    return (
+      <li className="pl-8">
+    		<span className={BGStyles.icon}><DragIcon type="primary" /></span>
+	      <ConstructorElement
+	        text={this.props.name}
+	        price={this.props.price}
+	        thumbnail={this.props.image}
+	      />
+      </li>
+    );
+  }
+}
+
+class BurgerConstructor extends React.Component {
+  render() {
+    return (
+			<section className="ml-10 pt-25">
+				<ul className={[BGStyles.inglist, "ml-4"].join(" ")} >
+		      <li className="pl-8">
+			      <ConstructorElement
+			        type="top"
+			        isLocked={true}
+			        text="Краторная булка N-200i (верх)"
+			        price={200}
+			        thumbnail={meatImg}
+			      />
+		      </li>
+		      <ul className={BGStyles.ajustable}>
+		      	{
+		      		utils.ingredients.map((product,index)=>{
+		      			let draggable = product.type !== "bun" ? true : false; 
+		      			return product.type === 'main' ? <Ingredient key={index} name={product.name} price={product.price} image={product.image} draggable={draggable} /> : null
+		      		})
+		      	}		
+			     </ul>
+			     <li className="pl-8">
+				      <ConstructorElement
+				        type="bottom"
+				        isLocked={true}
+				        text="Краторная булка N-200i (низ)"
+				        price={200}
+				        thumbnail={meatImg}
+				      />
+			     </li>
+		    </ul>
+		    <div className={[BGStyles.total, "mt-10"].join(" ")}>
+		    	<p className={[BGStyles.summ,"mr-10"].join(" ")}>
+		    		<span className={BGStyles.text}>610</span>
+		    		<span className={BGStyles.icon}><CurrencyIcon /></span>
+		    	</p>
+		    	<Button type="primary" size="large">
+					  Оформить заказ
+					</Button>
+		    </div>
+			</section>
+    );
+  }
+}
+
+export default BurgerConstructor;
---
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import BIStyles from './burger-ingredients.module.css';
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import * as utils from '../../utils/data.js';
+
+class IngredientsType extends React.Component {
+	render(){
+		return(
+				<>
+					<h2 className="mt-10 text_color_inactive">Соусы</h2>
+					<div className="pl-4">
+		      </div>
+	      </>
+			);
+	}
+}
+
+class Card extends React.Component {
+	render(){
+		return(
+				<div className={[BIStyles.card, "mb-10"].join(" ")}>
+					<a href="#" className={BIStyles.img} ><img src={this.props.image} /></a>
+					<p className={BIStyles.price}>{this.props.price}&nbsp;<CurrencyIcon type="primary" /></p>
+					<p className={BIStyles.name}>{this.props.name}</p>
+				</div>
+			);
+	}
+}
+
+class BurgerIngredients extends React.Component {
+
+  render() {
+    return (
+			<section>
+				<h1 className="mt-10">Соберите бургер</h1>
+				<ul className={[BIStyles.jumpTo, "mt-5"].join(" ")}>
+					<li className={BIStyles.active}>Булки</li>
+					<li>Соусы</li>
+					<li>Начинки</li>
+				</ul>
+
+				<div className={BIStyles.ingList}>
+					<h2 className="mt-10 mb-6">Булки</h2>
+					<div className="pl-4">
+						{
+		      		utils.ingredients.map((product,index)=>{
+		      			return product.type === 'bun' ? <Card key={index} name={product.name} price={product.price} image={product.image}/> : null
+		      		})
+		      	}	
+					</div>
+
+					<h2 className="mt-10 text_color_inactive">Соусы</h2>
+					<div className="pl-4">
+						{
+		      		utils.ingredients.map((product,index)=>{
+		      			return product.type === 'sauce' ? <Card key={index} name={product.name} price={product.price} image={product.image}/> : null
+		      		})
+		      	}	
+	      	</div>
+
+					<h2 className="mt-10 text_color_inactive">Начинки</h2>
+					<div className="pl-4">
+						{
+		      		utils.ingredients.map((product,index)=>{
+		      			return product.type === 'main' ? <Card key={index} name={product.name} price={product.price} image={product.image}/> : null
+		      		})
+		      	}	
+	      	</div>
+      	</div>
+
+			</section>
+    );
+  }
+}
+
+export default BurgerIngredients;
