727a1cb2dbef8d245fc205f2da06350fa6243669
Doroshin Nikolay <ndoroshin@HD-MacBook-02.Dlink>
2021-08-04 13:02:32 +0300

    get order number issue fix

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,13 +14,10 @@ import Modal from '../modal/modal';
 function App() {
 
 	const API_URL = 'https://norma.nomoreparties.space/api/ingredients';
-	const ORDER_URL = 'https://norma.nomoreparties.space/api/orders';
 
 	const [state,setState] = React.useState({
 		isLoading: true,
 		ingredients: [],
-		orderNumber: null,
-		orderLoading: true,
 	});
 
 	const [modalVisible, setVisible] = React.useState(false)
@@ -37,17 +34,17 @@ function App() {
 		      component = <IngredientDetails product={currProduct} />;
 		      break;
 		    case "order":
-		    	// рендерим компонент
-		    	component = <OrderDetails orderNumber={state.orderNumber} orderLoading={state.orderLoading} />;
-
+		    	
 		    	// собираем ID продуктов
 		    	var ingredientsIDs = [];
 		    	var ingredientsElements = document.getElementById('burgerconstructor').querySelectorAll('li.ingredient');
 		    	for (let i = 0; i < ingredientsElements.length; i++) {
 					  ingredientsIDs.push(JSON.parse(ingredientsElements[i].getAttribute('product'))._id)
 					}
-		    	// обращаемся к API, получаем номер заказа
-		    	getOrderNum(ingredientsIDs);
+
+					// рендерим компонент
+		    	component = <OrderDetails ingredientsIDs={{ingredientsIDs}} />;
+
 		    	break;
 		    default:
 		      component = 'Сюда можно поставить компонент-заглушку модалки';
@@ -58,28 +55,6 @@ function App() {
 		}
 		setVisible(!modalVisible);
 		event.preventDefault();
-	}
-
-	function getOrderNum(ingredientsIDs){
-		setState({ ...state, orderLoading: true });
-		const reqOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				'ingredients': ingredientsIDs
-			})
-		};
-		fetch(ORDER_URL, reqOptions)
-			.then(res => {
-			if (res.ok) {
-				return res.json();
-			}
-				return Promise.reject(`Ошибка ${res.status}`);
-			})
-			.then(data => {
-				setState({...state, orderNumber: data.order.number, orderLoading: false });
-			})
-			.catch(e => console.log('Error see can I in order number, my young padavan'));
 	}
 
 	function handleUserKeyPress(event) { 
--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
 import oDStyles from './order-details.module.css';
 
 function OrderDetails(props) {
 
-  React.useEffect(() => {
-    return () => {
-      console.log('change');
-     };
-  }, [props.orderLoading]);
+  const ORDER_URL = 'https://norma.nomoreparties.space/api/orders';
+  const [state,setState] = React.useState({
+    orderNumber: null,
+    orderLoading: true,
+  });
 
-  console.log(props.orderLoading);
+  function getOrderNum(ingredientsIDs){
+    const reqOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        'ingredients': ingredientsIDs
+      })
+    };
+    fetch(ORDER_URL, reqOptions)
+      .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+        return Promise.reject(`Ошибка ${res.status}`);
+      })
+      .then(data => {
+        setState({orderNumber: data.order.number, orderLoading: false });
+      })
+      .catch(e => console.log('Error see can I in order number, my young padavan'));
+  }
+
+  React.useEffect(() => {
+    getOrderNum(props.ingredientsIDs.ingredientsIDs);
+  }, []);
 
   return (
   	<div className={oDStyles.orderDetails} >
-      <p className={[oDStyles.orderId, "text_type_digits-large"].join(" ")} id="orderNum">{ props.orderLoading ? '...' : props.orderNumber}</p>
-      <span className={oDStyles.subline}>{ props.orderLoading && 'получаем ' }идентификатор заказа</span>
+      <p className={[oDStyles.orderId, "text_type_digits-large"].join(" ")} id="orderNum">{ state.orderLoading ? '...' : state.orderNumber}</p>
+      <span className={oDStyles.subline}>{ state.orderLoading && 'получаем ' }идентификатор заказа</span>
       <i className={oDStyles.statusIcon}></i>
   		<p className={oDStyles.detailedStatus}>
   			Ваш заказ начали готовить<br/>
@@ -26,8 +48,5 @@ function OrderDetails(props) {
   );
 }
 
-OrderDetails.propTypes = {
-  orderNumber: PropTypes.number,
-}
 
 export default OrderDetails;
