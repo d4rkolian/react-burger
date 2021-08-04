406a66a28c23155f4084856c9bf431b65d61f6b4
Doroshin Nikolay <ndoroshin@HD-MacBook-02.Dlink>
2021-08-04 12:38:29 +0300

    total cost update

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -4,6 +4,15 @@ import PropTypes from 'prop-types'
 import oDStyles from './order-details.module.css';
 
 function OrderDetails(props) {
+
+  React.useEffect(() => {
+    return () => {
+      console.log('change');
+     };
+  }, [props.orderLoading]);
+
+  console.log(props.orderLoading);
+
   return (
   	<div className={oDStyles.orderDetails} >
       <p className={[oDStyles.orderId, "text_type_digits-large"].join(" ")} id="orderNum">{ props.orderLoading ? '...' : props.orderNumber}</p>
